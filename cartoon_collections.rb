7c545351bbc23e3f7fa920312c78ab780c3d0475
Malcolm Leake <echelon1089@gmail.com>
2018-06-05 22:54:08 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,21 +5,22 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |dwarf, number|
     puts "#{number + 1}, #{dwarf}"
   end
-   end
+end
 
 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 
 def summon_captain_planet( planeteer_calls) 
-planeteer_calls.collect do |call|
-  "#{call.capitalize}!"
+  planeteer_calls.collect do |call|
+    "#{call.capitalize}!"
+  end
 end
-end
+
 short_words = ["puff", "go", "two"]
 
 def long_planeteer_calls(short_words)
   short_words.any? do |word|
-  word.length > 4
+    word.length > 4
 end
 end
 
