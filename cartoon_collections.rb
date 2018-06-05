0036914a8a3afa63ec1993df94c4be2150ad4754
Malcolm Leake <echelon1089@gmail.com>
2018-06-05 22:02:53 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -23,8 +23,8 @@ def long_planeteer_calls(short_words)
 end
 end
 
-def find_the_cheese(cheese) # code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese) 
+ 
   cheese.include?("cheddar", "gouda", "camembert")
 end
 
